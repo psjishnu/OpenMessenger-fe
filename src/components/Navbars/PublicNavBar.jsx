1855d6fd05fc96d44676509bc1da105fde669be0
psjishnu <psjishnu13@gmail.com>
2020-07-25 23:12:19 +0530

    fixed public navbar

--- src/components/Navbars/PublicNavBar.jsx
+++ src/components/Navbars/PublicNavBar.jsx
@@ -4,8 +4,8 @@ const pic = require("./simplechat.png");
 export default function PublicNavBar() {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-green-700">
-            <div className="">
-                <div className="w-full  md:w-1/3 lg:w-1/3">
+            <div className="md:w-1/2 w-full lg:w-1/2">
+                <div className="w-full  md:w-2/3 lg:w-1/3">
                     <A href="/">
                         <div className="text-white flex items-center mr-6 py-6 pl-6">
                             <img
